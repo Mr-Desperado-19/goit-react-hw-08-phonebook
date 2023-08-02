64f8e129c2868070826d36653aa0c524c01c651a
Mr-Desperado-19 <mr.arturhlebov@gmail.com>
2023-08-02 19:23:58 +0200

    update

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -2,18 +2,18 @@ import { useDispatch } from 'react-redux';
 import { lazy, useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react';
-import { Layout } from './Layout';
-import { PrivateRoute } from './PrivateRoute';
-import { RestrictedRoute } from './RestrictedRoute';
+import { Layout } from '../layout/Layout';
+import { PrivateRoute } from '../privateRoute/PrivateRoute';
+import { RestrictedRoute } from '../RestrictedRoute';
 import { refreshUser } from 'redux/auth/operations';
-import { Loader } from './loader/Loader';
+import { Loader } from '../loader/Loader';
 import { useAuth } from 'hooks';
 import { NotFound } from 'pages/NotFound';
 
-const HomePage = lazy(() => import('../pages/Home'));
-const RegisterPage = lazy(() => import('../pages/Register'));
-const LoginPage = lazy(() => import('../pages/Login'));
-const ContactsPage = lazy(() => import('../pages/Contacts'));
+const HomePage = lazy(() => import('../../pages/Home'));
+const RegisterPage = lazy(() => import('../../pages/Register'));
+const LoginPage = lazy(() => import('../../pages/Login'));
+const ContactsPage = lazy(() => import('../../pages/Contacts'));
 
 export const App = () => {
   const dispatch = useDispatch();
